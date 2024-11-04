c0c07fa5be8b00b0d5c67e94d468c172337eeaf3
Matthew <118353174+matthewInforma@users.noreply.github.com>
2024-11-05 00:32:01 +0100

    feat: apply theme & date provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import Homepage from "./pages/HomePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { DateProvider } from "./providers/date/dateProvider";
+import { ThemeProvider } from "./providers/theme/themeProvider";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -11,7 +13,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Homepage />
+      <ThemeProvider>
+        <DateProvider>
+          <Homepage />
+        </DateProvider>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
